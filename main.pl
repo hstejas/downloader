0d1966d309430de5c2af8281dc7ab49e24af2906
hstejas <hstejas@users.noreply.github.com>
2014-07-15 16:33:06 +0530

    Update main.pl

--- main.pl
+++ main.pl
@@ -8,6 +8,28 @@ use LWP::Simple;
 my @websites = @ARGV;
 my $argLen = @ARGV;
 
+
+my $destFolder="./";
+
+
 warn "No Arguments Provided\n" if ($argLen == 0);
 
+sub getImageUrisFromFacebook($);
+
+
 while(@websites)
+{
+  my $response = get;  
+  
+  @imageUris =  getImageUrisFromFaceBook($response);
+}
+
+
+sub getImageUrisFromFaceBook()
+{
+  my $url =   shift;
+  my @ret;
+  @ret = $url~=m/url\(()\)/g;
+  
+  return @ret;
+}
